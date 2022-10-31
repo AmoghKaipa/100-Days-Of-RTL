7c5be6b5396eeb2ca5a7b5884f3a89dd1fa63a5b
Amogh Kaipa <amoghkaipa@gmail.com>
2022-10-31 07:56:40 +0530

    Priority arbiter without starvation
    // Priority arbiter without starvation
    // Arbiter grants access to the device with the highest priority for a clock cycle.
    // But, if the same device was granted access in the previous clock cycle,
    // the device with the next priority is given access in the present clock cycle
    // Day 14 module is also used here to find out the grant signal based on the priority

--- Day 15/day_15.sv
+++ Day 15/day_15.sv
@@ -1,6 +1,12 @@
 `timescale 1ns / 1ps
-// Round robin arbiter
 
+// Priority arbiter without starvation
+// Arbiter grants access to the device with the highest priority for a clock cycle. 
+// But, if the same device was granted access in the previous clock cycle, 
+// the device with the next priority is given access in the present clock cycle
+// Day 14 module is also used here to find out the grant signal based on the priority
+
+ 
 module day15 (
   input     wire        clk,
   input     wire        reset,
@@ -20,6 +26,7 @@ module day15 (
       mask_q <= nxt_mask;
 
   // Next mask based on the current grant
+  // Mask is used so that the same device is not granted access in the next clock cycle also
   always_comb begin
     nxt_mask = mask_q;
          if (gnt_o[0]) nxt_mask = 4'b1110;
@@ -36,10 +43,13 @@ module day15 (
   logic [3:0] mask_gnt;
   logic [3:0] raw_gnt;
   // Generate grants for req and masked req
+  // Need to generate maskedGnt to avoid starvation
+  // Need to generate rawGnt in case the same device alone requested for access in both cycles
   day_14 #(4) maskedGnt (.req_i (mask_req), .gnt_o (mask_gnt));
   day_14 #(4) rawGnt    (.req_i (req_i),    .gnt_o (raw_gnt));
 
   // Final grant based on mask req
+  
   assign gnt_o = |mask_req ? mask_gnt : raw_gnt;
 
 endmodule
