c08e8b4ad6267d60ed61d81aba2ce00649742b99
Amogh Kaipa <amoghkaipa@gmail.com>
2022-11-08 22:31:52 +0530

    Day 20

---
+++ Day 20/day_20_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/08/2022 10:22:53 PM
+// Design Name: 
+// Module Name: day_20_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module day_20_tb(
+
+    );
+endmodule
